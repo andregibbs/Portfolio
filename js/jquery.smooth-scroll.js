742a7e2efcabfc086a94c56b81df2cf6b64b9a5a
gassedgibbs <andre_d_gibbs@hotmail.com>
2015-01-03 00:06:32 +0000

    Whole Site
    First Commit

---
+++ js/jquery.smooth-scroll.js
@@ -0,0 +1,23 @@
+$('a[href^="#"]').on('click',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 2000, 'swing', function () {
+window.location.hash = target;
+});
+});$('a[href^="#"]').on('click',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 2000, 'swing', function () {
+window.location.hash = target;
+});
+});
